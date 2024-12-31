24853c62957e9fbcb6a5a35cb4b4043a9baca832
Suraj.Adhikari <suraj.adhikarifullstacklabs@digital>
2024-12-31 11:01:02 -0500

    aop setup

---
+++ my-app/src/main/java/io/reactivestax/aop/LoggingAspect.java
@@ -0,0 +1,15 @@
+package io.reactivestax.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@EnableAspectJAutoProxy
+public class LoggingAspect {
+
+    @Pointcut("exec")
+    public void logAroundPointCutDefinition(){}
+}
